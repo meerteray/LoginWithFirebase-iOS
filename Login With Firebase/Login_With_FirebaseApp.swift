f98beca879a31a13c11f9a659c70dc98b7567569
Mert Eray <meerteray@gmail.com>
2023-08-29 16:52:54 +0300

    Initial Commit

---
+++ Login With Firebase/Login_With_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_With_FirebaseApp.swift
+//  Login With Firebase
+//
+//  Created by Mert Eray on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Login_With_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
