dc919bda6da668cf73e6a7b1300081ce32c02730
Mert Eray <meerteray@gmail.com>
2023-08-31 15:34:37 +0300

    ui design

--- Login With Firebase/ContentView.swift
+++ Login With Firebase/ContentView.swift
@@ -1,26 +1,104 @@
-//
-//  ContentView.swift
-//  Login With Firebase
-//
-//  Created by Mert Eray on 29.08.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.pink, .red, .blue], startPoint:
+                        .topLeading, endPoint: .bottomTrailing))
+            
+                    .frame(width: 1000, height: 400)
+                    .rotationEffect(.degrees(135))
+                    .offset(y: -560)
+            
+        VStack(spacing: 20) {
+                Text("Welcome")
+                    .foregroundColor( .white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(x: -100, y: 0)
+            
+            TextField("Email", text: $email)
+                .foregroundColor(.white)
+                .textFieldStyle(.plain)
+                .placeholder(when: email.isEmpty) {
+                    Text("Email")
+                        .foregroundColor(.white)
+                        .bold()
+                }
+            
+            Rectangle()
+                .frame(width: 350, height: 1)
+                .foregroundColor(.white)
+            
+            SecureField("Password", text: $password)
+                .foregroundColor(.white)
+                .textFieldStyle(.plain)
+                .placeholder(when: password.isEmpty) {
+                    Text("Password")
+                        .foregroundColor(.white)
+                        .bold()
+                }
+            
+            Rectangle()
+                .frame(width: 350, height: 1)
+                .foregroundColor(.white)
+            
+            
+            Button {
+                // sign up
+            } label:  {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width: 200, height: 40)
+                        .background(
+                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(.linearGradient(colors: [.white], startPoint:
+                                .top, endPoint: .bottomTrailing))
+                        )
+                        .foregroundColor(.black)
+                }
+            .padding(.top)
+            .offset(y: 110)
+            
+            Button {
+                // login
+            } label:  {
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundColor(.white)
+                }
+            .padding(.top)
+            .offset(y: 110)
+            
+            }
+        .frame(width: 350)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension View {
+    
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+            
+            ZStack(alignment: alignment) {
+                placeholder().opacity(shouldShow ? 1 : 0)
+                self
+            }
+        }
 }
--- Login With Firebase/Login_With_FirebaseApp.swift
+++ Login With Firebase/Login_With_FirebaseApp.swift
@@ -1,10 +1,3 @@
-//
-//  Login_With_FirebaseApp.swift
-//  Login With Firebase
-//
-//  Created by Mert Eray on 29.08.2023.
-//
-
 import SwiftUI
 
 @main
