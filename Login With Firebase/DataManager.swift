e8346969aa0db3466fb26971fd6b9a45e081b335
Mert Eray <meerteray@gmail.com>
2023-09-03 23:32:02 +0300

    fetchDogs func added

--- Login With Firebase/DataManager.swift
+++ Login With Firebase/DataManager.swift
@@ -6,7 +6,17 @@ class DataManager: ObservableObject {
     @Published var dogs: [Dog] = []
     
     func fetchDogs() {
+        dogs.removeAll()
         
+        let database = Firestore.firestore()
+        let ref = database.collection("Dogs")
+        ref.getDocuments { snapshot, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+        }
     }
     
 }
