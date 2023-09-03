6cb8111b691378d824c7c183d325e6412b1dc49c
Mert Eray <meerteray@gmail.com>
2023-09-03 23:16:32 +0300

    new file

--- Login With Firebase/ContentView.swift
+++ Login With Firebase/ContentView.swift
@@ -5,8 +5,19 @@ struct ContentView: View {
     
     @State private var email = ""
     @State private var password = ""
+    @State private var userIsLoggedIn = false
+    
     
     var body: some View {
+
+        if userIsLoggedIn {
+            ListView()
+        } else {
+            content
+        }
+    }
+    
+    var content: some View {
         ZStack {
             Color.black
             
@@ -14,72 +25,80 @@ struct ContentView: View {
                 .foregroundStyle(.linearGradient(colors: [.pink, .red, .blue], startPoint:
                         .topLeading, endPoint: .bottomTrailing))
             
-                    .frame(width: 1000, height: 400)
-                    .rotationEffect(.degrees(135))
-                    .offset(y: -560)
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -560)
             
-        VStack(spacing: 20) {
+            VStack(spacing: 20) {
                 Text("Welcome")
                     .foregroundColor( .white)
                     .font(.system(size: 40, weight: .bold, design: .rounded))
                     .offset(x: -100, y: 0)
-            
-            TextField("Email", text: $email)
-                .foregroundColor(.white)
-                .textFieldStyle(.plain)
-                .placeholder(when: email.isEmpty) {
-                    Text("Email")
-                        .foregroundColor(.white)
-                        .bold()
-                }
-            
-            Rectangle()
-                .frame(width: 350, height: 1)
-                .foregroundColor(.white)
-            
-            SecureField("Password", text: $password)
-                .foregroundColor(.white)
-                .textFieldStyle(.plain)
-                .placeholder(when: password.isEmpty) {
-                    Text("Password")
-                        .foregroundColor(.white)
-                        .bold()
-                }
-            
-            Rectangle()
-                .frame(width: 350, height: 1)
-                .foregroundColor(.white)
-            
-            Button {
-                register()
-            } label:  {
+                
+                TextField("Email", text: $email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty) {
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty) {
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width: 350, height: 1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    register()
+                } label:  {
                     Text("Sign up")
                         .bold()
                         .frame(width: 200, height: 40)
                         .background(
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .fill(.linearGradient(colors: [.white], startPoint:
-                                .top, endPoint: .bottomTrailing))
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(.linearGradient(colors: [.white], startPoint:
+                                        .top, endPoint: .bottomTrailing))
                         )
                         .foregroundColor(.black)
                 }
-            .padding(.top)
-            .offset(y: 110)
-            
-            Button {
-                login()
-            } label:  {
+                .padding(.top)
+                .offset(y: 110)
+                
+                Button {
+                    login()
+                } label:  {
                     Text("Already have an account? Login")
                         .bold()
                         .foregroundColor(.white)
                 }
-            .padding(.top)
-            .offset(y: 110)
-            
+                .padding(.top)
+                .offset(y: 110)
+                
             }
-        .frame(width: 350)
+            .frame(width: 350)
+            .onAppear() {
+                Auth.auth().addStateDidChangeListener { auth, user in
+                    if user != nil {
+                        userIsLoggedIn.toggle()
+                    }
+                }
+            }
         }
         .ignoresSafeArea()
+
     }
     
     func login() {
---
+++ Login With Firebase/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Login With Firebase
+//
+//  Created by Mert Eray on 1.09.2023.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
