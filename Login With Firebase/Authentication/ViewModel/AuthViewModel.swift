486d0724bb03e7432124f9ce261c8db9e523763e
Mert Eray <meerteray@gmail.com>
2023-09-09 15:04:20 +0300

    fetchUser data

--- Login With Firebase/Authentication/ViewModel/AuthViewModel.swift
+++ Login With Firebase/Authentication/ViewModel/AuthViewModel.swift
@@ -2,12 +2,17 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
+@MainActor
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
     
     init() {
+        self.userSession = Auth.auth().currentUser
         
+        Task {
+            await fetchUser()
+        }
     }
     
     func signIn(withEmail email: String, password: String) async throws {
@@ -21,6 +26,7 @@ class AuthViewModel: ObservableObject {
             let user = User(id: result.user.uid, fullname: fullname, email: email)
             let encodedUser = try Firestore.Encoder().encode(user)
             try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            await fetchUser()
         } catch {
             print("Debug: Failed to create user with error \(error.localizedDescription)")
         }
@@ -31,6 +37,10 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() async {
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
+        self.currentUser = try? snapshot.data(as: User.self)
         
     }
 }
