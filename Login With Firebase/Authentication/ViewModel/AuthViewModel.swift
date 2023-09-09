4331f6979257efa03e3cf5bf4dd65d050980a52c
Mert Eray <meerteray@gmail.com>
2023-09-09 14:33:13 +0300

    createUser func added

--- Login With Firebase/Authentication/ViewModel/AuthViewModel.swift
+++ Login With Firebase/Authentication/ViewModel/AuthViewModel.swift
@@ -15,7 +15,15 @@ class AuthViewModel: ObservableObject {
     }
     
     func createUser(withEmail email: String, password: String, fullname: String) async throws {
-       print("Create User")
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = User(id: result.user.uid, fullname: fullname, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch {
+            print("Debug: Failed to create user with error \(error.localizedDescription)")
+        }
     }
     
     func signOut() {
--- Login With Firebase/Login_With_FirebaseApp.swift
+++ Login With Firebase/Login_With_FirebaseApp.swift
@@ -1,8 +1,14 @@
 import SwiftUI
+import Firebase
 
 @main
 struct Login_With_FirebaseApp: App {
     @StateObject var viewModel = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
